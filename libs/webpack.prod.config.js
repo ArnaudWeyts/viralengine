f988e5aeff4d8b6f13fcb703d506ba3ac1cedc3f
Arnaud Weyts <arnaud.weyts@gmail.com>
2016-12-15 21:30:00 +0100

    changed production config

--- libs/webpack.prod.config.js
+++ libs/webpack.prod.config.js
@@ -16,6 +16,11 @@ module.exports = {
     filename: 'main.min.js'
   },
   plugins: [
+    new webpack.DefinePlugin({
+      'process.env': {
+        NODE_ENV: JSON.stringify('production')
+      }
+    }),
     new webpack.optimize.DedupePlugin(),
     new webpack.optimize.UglifyJsPlugin()
   ],
