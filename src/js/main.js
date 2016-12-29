d08de41dc502e87d631ad89273148373e5d4dd2b
Arnaud Weyts <arnaud.weyts@gmail.com>
2016-12-29 21:07:29 +0100

    changed to create-react-app & ejected config right away 👏

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ import injectTapEventPlugin from 'react-tap-event-plugin';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import AppBarHeader from './header'
+import AppBarHeader from 'header'
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
